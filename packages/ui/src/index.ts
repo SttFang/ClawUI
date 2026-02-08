73b391d728dccac48c8f1cecf706c84e5db73888
fanghanjun <17737663888@163.com>
2026-02-09 00:04:56 +0800

    ✨ feat: extract UI components to @clawui/ui package

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,26 @@
+// Utils
+export { cn } from './utils'
+
+// Components
+export { Button, buttonVariants, type ButtonProps } from './button'
+export {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from './card'
+export { Input, type InputProps } from './input'
+export { Label, type LabelProps } from './label'
+export { ScrollArea, ScrollBar } from './scroll-area'
+export { Switch, type SwitchProps } from './switch'
+export {
+  Tabs,
+  TabsList,
+  TabsTrigger,
+  TabsContent,
+  type TabsProps,
+  type TabsTriggerProps,
+  type TabsContentProps,
+} from './tabs'
